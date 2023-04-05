cb1a83bbe39ac378228aefef1581d27a9e370b2b
JoKwanhee <jonotch1@naver.com>
2023-04-06 04:00:54 +0900

    Add BaseXXX - activity

---
+++ Base/app/src/main/java/com/bcsd/base/BaseActivity.kt
@@ -0,0 +1,33 @@
+package com.bcsd.base
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+
+abstract class BaseActivity<T : ViewDataBinding>: AppCompatActivity() {
+    abstract val layoutId: Int
+    private lateinit var _binding: T
+
+    val binding: T
+        get() = _binding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        _binding = DataBindingUtil.setContentView(this, layoutId)
+        setContentView(binding.root)
+        initState()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding.unbind()
+    }
+
+    abstract fun initView()
+    abstract fun initEvent()
+    open fun initState(){
+        initView()
+        initEvent()
+    }
+}
---
+++ Base/app/src/main/java/com/bcsd/base/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.bcsd.base
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.bcsd.base.databinding.ActivityMainBinding
+
+class MainActivity : BaseActivity<ActivityMainBinding>() {
+    override val layoutId: Int
+        get() = R.layout.activity_main
+
+    override fun initView() {
+        TODO("Not yet implemented")
+    }
+
+    override fun initEvent() {
+        TODO("Not yet implemented")
+    }
+}
